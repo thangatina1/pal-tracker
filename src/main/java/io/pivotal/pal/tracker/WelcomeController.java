1d9b84d9d7e4b0c6faf43e9da4a0f0358b8afa23
thangatina1 <montfort1A>
2020-03-21 23:48:40 -0400

    Starting Assignment 2

--- src/main/java/io/pivotal/pal/tracker/WelcomeController.java
+++ src/main/java/io/pivotal/pal/tracker/WelcomeController.java
@@ -1,13 +1,22 @@
 package io.pivotal.pal.tracker;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class WelcomeController {
 
-    @GetMapping("/")
-    public String sayHello() {
-        return "hello";
+    private String displayMessage;
+
+    public WelcomeController( @Value("${welcome.message}") String displayMessage)
+    {
+        this.displayMessage = displayMessage;
     }
+
+    @GetMapping("/")
+    public String sayHello(){
+        return(displayMessage);
+    }
+
 }
